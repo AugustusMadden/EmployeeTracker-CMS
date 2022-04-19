1a008e4a72c63953d417d561bb73b53a1c410dbb
Augustus Madden <gusmadden@gmail.com>
2022-04-19 16:25:40 -0400

    Updated server.js, merged with functionality from index

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,20 @@
-INSERT INTO department (id,name)
-VALUES (),
-();
+INSERT INTO department (name)
+VALUES ("Shipping"),
+("Management"),
+("Production"),
+("QA")
+;
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (),
-();
+INSERT INTO role (title, salary)
+VALUES ("Manager", 2,000),
+("Assistant Manager", 20),
+("Key Grip", 200,000),
+("Director", 20,000)
+;
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (),
-();
+INSERT INTO employee (first_name, last_name)
+VALUES ("Margo", "Melanija"),
+( "Clara", "Olov" ),
+( "Marija", "Glen" ),
+( "Sung-Hyun", "Woodcock" )
+;
--- server.js
+++ server.js
@@ -1,29 +1,153 @@
-const express = require('express');
 const mysql = require('mysql2');
 
-const PORT = process.env.PORT || 3001
-const app = express();
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-app.use(express.urlencoded({ extended: false }));
+const randID = require('./utils/randID')
+//IMPORT questions from index.js
+
+/* app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+); */
 
-const db = mysql.createConnection(
+    const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: '',
+        password: 'MaddenSQL_alpha903',
         database: 'employeelist_db'
     },
     console.log(`Connected to the employeelist_db database.`)
-)
+);
 
-app.listen(PORT, () => {
+db.connect((err) => {
+    if (err) {
+      return console.error('error: ' + err.message);
+    }
+});
+function init() {
+    //Load all seed data into schema tables on initiation
+    
+     inquirer
+        .prompt(
+            {
+                type: 'list',
+                name: 'startSelect',
+                message: 'What would you like to do today?',
+                choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+                
+            }
+        )
+
+        .then((data) => {
+            console.log(data);
+
+            if (data.startSelect === 'View all departments') {
+                    const sql = `SELECT * FROM department`;
+
+                    db.query(sql, (err, rows) => {
+                    });
+            } 
+            else if (data.startSelect === 'View all roles') {
+                const sql = `SELECT * FROM roles`;
+
+                db.query(sql, (err, rows) => {
+                });
+            } 
+            else if (data.startSelect === 'View all employees') {
+                const sql = `SELECT * FROM roles`;
+
+                db.query(sql, (err, rows) => {
+                });
+            } 
+            else if (data.startSelect === 'Add a department') {
+                inquirer
+                    .prompt(deptQuestion)
+                    .then((data) => {
+                        console.log(data)
+                        init();
+                    });
+            } 
+            else if (data.startSelect === 'Add a role') {
+                inquirer
+                    .prompt(roleQuestion)
+                    .then((data) => {
+                        console.log(data)
+                    });
+            } 
+            else if (data.startSelect === 'Add an employee') {
+                inquirer
+                    .prompt(empQuestion)
+                    .then((data) => {
+                        console.log(data)
+                    });
+            }
+            else if (data.startSelect === 'Update an employee role') {
+                return ;
+            }
+        });
+
+}
+
+const deptQuestion = [{
+    type: 'input',
+    name: 'deptName',
+    message: 'Please enter the name of the new department.'
+}];
+
+
+const roleQuestion = [
+    {
+    type: 'input',
+    name: 'roleName',
+    message: 'Please enter the name of the new role.'
+    },  
+    {type: 'input',
+    name: 'roleSalary',
+    message: 'Please enter the salary of the new role.'
+    },  
+    {
+    type: 'input',
+    name: 'roleDept',
+    message: 'Please enter the department of the new role.'
+    }
+];
+
+
+const empQuestion = [{
+    type: 'input',
+    name: 'empFirstName',
+    message: 'Please enter the first name of the new employee.'
+    },
+    {
+    type: 'input',
+    name: 'empLastName',
+    message: 'Please enter the last name of the new employee.'
+    },
+    {
+    type: 'input',
+    name: 'empRole',
+    message: 'Please enter the role of the new employee.'
+    },
+    {
+    type: 'input',
+    name: 'empManager',
+    message: 'Please enter the name of the manager new employee.'
+    }
+]
+
+
+/* WHEN I choose to update an employee role
+THEN I am prompted to select an employee to update and their new role and this information is updated in the database */
+
+
+/* app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-})
+}) */
 
+init();
